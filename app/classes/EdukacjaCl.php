57f7ae0382886dc120da472f7d246b83459da37b
erbear <erbert.b@gmail.com>
2014-02-02 20:26:15 +0100

    po zmianie EdukacjaCL

--- app/classes/EdukacjaCl.php
+++ app/classes/EdukacjaCl.php
@@ -1,5 +1,5 @@
 <?php
-//require('phpquery/phpQuery/phpQuery.php');
+require('phpQuery.php');
 
 class EdukacjaCl 
 {
@@ -7,15 +7,21 @@ class EdukacjaCl
 	private $ch;
 	private $HTML;
 	private $URL= [
+		'default'=> "https://edukacja.pwr.wroc.pl",
 		'home' => "https://edukacja.pwr.wroc.pl/EdukacjaWeb/studia.do",
 		'logIn' => "https://edukacja.pwr.wroc.pl/EdukacjaWeb/logInUser.do",
-		'zapisy' => "https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?href=#hrefZapisySzczSlu"
+		'zapisy' => "https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?href=#hrefZapisySzczSlu",
+		'przegladanieGrup'=>"https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do",
+		'przegladanieZWektora'=>"https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?event=ZapiszFiltr&event=wyborKryterium&href=#hrefKryteriumFiltrowania",
+		'kolejnaStrona'=>"https://edukacja.pwr.wroc.pl/EdukacjaWeb/zapisy.do?href="
 	];
 	private $UrlEvents = [
 		''
 	];
 	private $dataToSend;
 	private $courses;
+	private $dates;
+	private $kursyDoZapisow;
 	private $semestr;
 	private $dane = [];
 	public function EdukacjaCl($user, $password)
@@ -30,6 +36,7 @@ class EdukacjaCl
 		curl_setopt($this->ch, CURLOPT_VERBOSE, 1);
 		curl_setopt($this->ch, CURLOPT_HEADER, 0);  
 		curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, 120);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, true);
 
@@ -39,6 +46,7 @@ class EdukacjaCl
 	function __destruct(){
 		curl_close($this->ch);
 	}
+
 	private function loadPage()
 	{
 		$this->HTML = curl_exec($this->ch);
@@ -67,6 +75,7 @@ class EdukacjaCl
 	private function clearData(){
 		$this->dataToSend = [];
 	}
+	//buduje URL z danych
 	private function dataToUrl(){
 		$dataProcessed='';
 		foreach ($this->dataToSend as $key =>$value){
@@ -88,11 +97,13 @@ class EdukacjaCl
 	public function getDane(){
 		return $this->dane;
 	}
+	//pobiera tokeny
 	private function getTokens(){
 		$doc = phpQuery::newDocumentHtml($this->HTML);
 		$this->dataToSend['clEduWebSESSIONTOKEN'] = pq('input[name="clEduWebSESSIONTOKEN"]')->attr('value');
 		$this->dataToSend['cl.edu.web.TOKEN'] = pq('input[name="cl.edu.web.TOKEN"]')->attr('value');
 	}
+	//pobiera URL z pozycji z menu
 	private function getUrl($title){
 		$doc = phpQuery::newDocumentHtml($this->HTML);
 		$urlPath = "https://edukacja.pwr.wroc.pl" . pq('.GUZIK[title="'.$title.'"]')->attr('href');
@@ -164,6 +175,42 @@ class EdukacjaCl
 		}
 		
 	}
+	public function getDataFromWektor(){
+		$doc = phpQuery::newDocumentHtml($this->HTML);
+
+		preg_match_all('/>\s*[a-zA-Z]{1}\d{2}-\d{1,2}[a-zA-Z]{1}/i', $this->HTML, $matches);
+
+		foreach ($matches as $m){
+			foreach ($m as $key=>$match){
+				$pos1 = strrpos($this->HTML, $match);
+				$pos2 = (isset($m[$key+1])?$m[$key+1]:0) ? strrpos($this->HTML, isset($m[$key+1])?$m[$key+1]:0) : strlen($this->HTML);
+				$course = substr($this->HTML, $pos1, ($pos2-$pos1));
+				$course = '<tr><td'. $course;
+				$doc = phpQuery::newDocumentHtml($course);
+				preg_match('/(pn|wt|śr|cz|pt)(\/(TP|TN))?\s+(\d{2}):(\d{2})-(\d{2}):(\d{2}),?\s(bud.)?\s([A-Z]+-\d+),?\s(sala)?\s([^\s]+)/i', $course, $timestapmps);
+				if(count($timestapmps) == 0)
+				{
+					continue;	
+				}
+				$wiersz1 = pq('td:contains(' . trim(str_replace('>', ' ', $match)) .')')->parent('tr');
+				$miejsca = preg_split('/\//', trim($wiersz1['td:eq(3)']->text()));
+				$this->dates[] = ["nazwa" => trim($wiersz1['td:eq(2)']->text()),
+									"prowadzacy" => trim($wiersz1->nextAll('tr')->eq(0)->find('td:eq(0)')->text()),
+									"rodzaj" => trim($wiersz1->nextAll('tr')->eq(0)->find('td:eq(1)')->text()),		
+									"kod" => trim(str_replace('>', ' ', $match)),
+									"zajete" => $miejsca[0],
+									"wszystkie" => $miejsca[1],
+									"dzien" => trim($timestapmps[1]),
+									"tydzien" => trim($timestapmps[3]),
+									"start" => trim($timestapmps[4]) . ":" . trim($timestapmps[5]),
+									"koniec" => trim($timestapmps[6]) . ":" . trim($timestapmps[7]),
+									"budynek" => trim($timestapmps[9]),
+									"sala" => trim($timestapmps[11]),
+									"semestr" => trim($this->semestr)];
+			}
+		}
+		$doc = phpQuery::newDocumentHtml($this->HTML);
+	}
 	public function getCourses(){
 		return $this->courses;
 	}
@@ -175,6 +222,103 @@ class EdukacjaCl
 		$this->getDataFromSchedule();
 		return $this->getCourses();
 	}
-}
+	public function idzDoPrzegladaniaKursow(){
+		$this->logIn();
+		$this->goToPathFromMenu('Zapisy');
+		$this->wybierzSemestr();
 
+		//pobieram dane zeby dostac sie do przegladania grup
+
+		$this->getTokens();
+		$doc = phpQuery::newDocumentHtml($this->HTML);
+		$input1 = pq('input[name="event_ZapisyPrzegladanieGrup"]')->parent('td')->find('input:eq(0)');
+		$input2 = pq('input[name="event_ZapisyPrzegladanieGrup"]')->parent('td')->find('input:eq(1)');
+		$this->dataToSend[$input1->attr('name')] = $input1->attr('value');
+		$this->dataToSend[$input2->attr('name')] = $input2->attr('value');
+		$this->dataToSend['event'] = 'ZapisyPrzegladanieGrup';
+		$this->setPOST(true);
+		curl_setopt($this->ch, CURLOPT_URL, $this->URL['przegladanieGrup']);
+		$this->loadPage();
+
+		//pobieram dane zeby dostac sie do przegladania grup z wektora
+
+		$this->getTokens();
+		$this->dataToSend['KryteriumFiltrowania'] = 'Z_WEKTORA_ZAP';
+		$this->setPOST(true);
+		curl_setopt($this->ch, CURLOPT_URL, $this->URL['przegladanieZWektora']);
+		$this->loadPage();
+
+		//przemieszczam sie pomiedzy kursami
+
+		$this->setPOST(false);
+		$this->getTokens();
+		$doc = phpQuery::newDocumentHtml($this->HTML);
+		$odnosniki = pq("a[title='Wybierz wiersz']");
+		$nazwaKursu = [];
+		
+		//pobieranie adresow do wszystkich kursow
+
+		$pagings = pq(".paging-numeric-span")->eq(0)->find('input');//ilosc odnosnikow do zminy strony
+		$odnosnik = [];//adresy stron z danym kursem
+		$pagingRange = -10;
+		$numerKursu = 0;
+		foreach ($pagings as $key=>$paging){//foreach po stronach z grupami kursow
+			$hiddenInputs = pq(".paging-panel")->eq(0)->find('input:hidden');//wszystkie inputy ktore sa potrzebne do wyslania zapytania post
+			//pobieranie inputow do zmiennej z danymi
+			foreach($hiddenInputs as $hiddenInput){
+				$this->dataToSend[pq($hiddenInput)->attr('name')] = pq($hiddenInput)->attr('value');
+			}
+			$this->getTokens();
+			$this->dataToSend['pagingRangeStart'] = strval($pagingRange+=10); //ustawienie do ktorej strony ma byc przekierowanie
+			//wyslanie zapytania do danej strony
+			$this->setPOST(true);
+			curl_setopt($this->ch, CURLOPT_URL, $this->URL['kolejnaStrona']);
+			$this->loadPage();
+			$doc = phpQuery::newDocumentHtml($this->HTML);
+
+
+			$odnosniki = pq("a[title='Wybierz wiersz']");
+			foreach ($odnosniki as $numer=>$o){
+				$odnosnik[$numer] = ['kod'=>trim(pq($o)->text()),
+								 'href'=> pq($o)->attr('href'),
+								 'nazwa'=> trim(pq($o)->parent('td')->parent('tr')->find('td:eq(1)')->text())];
+			}
+			foreach($odnosnik as $o){
+				$this->getTokens();
+				$this->setPOST(false);
+				curl_setopt($this->ch, CURLOPT_URL, $this->URL['default'] . $o['href']);
+				$this->loadPage();
+				$numerStrony = 0;
+				$this->getDataFromWektor();
+				foreach (pq(".paging-numeric-span")->eq(1)->find('input:not(input:first-child))') as $input){
+					$hiddenInputs = pq(".paging-panel")->eq(1)->find('input:hidden');
+					foreach($hiddenInputs as $hiddenInput){
+						$this->dataToSend[pq($hiddenInput)->attr('name')] = pq($hiddenInput)->attr('value');
+					}
+					$this->getTokens();
+					$this->dataToSend['pagingRangeStart'] = strval($numerStrony+=10);
+					$this->setPOST(true);
+					curl_setopt($this->ch, CURLOPT_URL, $this->URL['kolejnaStrona']);
+					$this->loadPage();
+					$this->getDataFromWektor();
+				}
+				$this->kursyDoZapisow[] = ['nazwa' => $o['nazwa'], 'kod'=>$o['kod'], 'dane'=>$this->dates];
+				$this->dates = '';
+				$numerKursu++;
+			}
+			$odnosnik = '';
+		}
+
+
+		
+		return $this->kursyDoZapisow;
+		
+	}
+	public function getOpisStudiow(){
+		$this->logIn();
+		$this->goToPathFromMenu('Akademiki');
+		$doc = phpQuery::newDocumentHtml($this->HTML);
+		return trim(pq('.CENTER_TRESC')->find('table')->find('tr:eq(1)')->find('td:eq(1)')->html());
+	}
+}
 ?>
